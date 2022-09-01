853b91ff73dd27195b3971fba0f31bd3e89a2022
corno <corno.astn.a@schraverus.com>
2022-09-01 16:14:17 +0200

    .

--- test/src/bin/index.ts
+++ test/src/bin/index.ts
@@ -2,6 +2,7 @@
 import * as pe from "pareto-core-exe"
 import * as pa from "pareto-core-async"
 import * as pw from "pareto-core-raw"
+import * as pl from "pareto-core-lib"
 
 import * as diff from "res-pareto-diff"
 import * as fs from "res-pareto-filesystem"
@@ -12,13 +13,15 @@ pe.runProgram(
     pub.createTester(
         {
             getTestSet: () => {
+                //test that a failing test indeed fails!!! now it will make the program exit with an error code
+                pl.logDebugMessage("FIXME: TEST THE TESTLIB")
                 return pa.value({
                     elements: pw.wrapRawDictionary<pub.TTestElement>({
                         "a": {
                             type: ["test", {
                                 type: ["simple string", {
-                                    actual: "FOO",
-                                    expected: "BAR"
+                                    actual: "TODO: ACTUALLY TEST THE TEST LIB",
+                                    expected: "TODO: ACTUALLY TEST THE TEST LIB"
                                 }]
                             }]
                         }
