876162b2b191eba51f4433b89b56615a374108b6
corno <corno.astn.a@schraverus.com>
2023-04-11 16:21:06 +0200

    .

--- typescript/pub/src/bindings/implementations/createTestProgram.b.ts
+++ typescript/pub/src/bindings/implementations/createTestProgram.b.ts
@@ -17,58 +17,60 @@ import * as a_private from "../../submodules/private"
 import { A } from "../api.generated"
 
 export const $$: A.createTestProgram = ($d) => {
-    return () => {
-        const errorLogger = a_mainlib.$r.createErrorLogger()
-        const logger = a_mainlib.$r.createLogger()
-        const failedFlagger = a_mainlib.$r.setExitCodeToFailed()
-        return a_main.$a.createTestProgram({
-            'createTester': a_private.$a.createTester({
-                'isZero': a_boolean.$r.isZero(),
-                'runTests': a_private.$a.createTestRunner({
-                    'diffData': a_diff.$r.diffData(),
-                    'stringsAreEqual': a_diff.$r.stringsAreEqual(),
-                    'validateFile': a_private.$a.validateFile({
+    return {
+        'construct': () => {
+            const errorLogger = a_mainlib.$r.createErrorLogger()
+            const logger = a_mainlib.$r.createLogger()
+            const failedFlagger = a_mainlib.$r.setExitCodeToFailed()
+            return a_main.$a.createTestProgram({
+                'createTester': a_private.$a.createTester({
+                    'isZero': a_boolean.$r.isZero(),
+                    'runTests': a_private.$a.createTestRunner({
                         'diffData': a_diff.$r.diffData(),
-                        'readFile': () => {
-                            pl.panic("SFSDFSDF")
-                        },
-                    }, {
-                        'unlink': a_fs.$a.createUnlinkFireAndForget({
-                            'unlink': a_fsr.$r.unlink()
-                        })({
-                            'errorHandler': a_fserror.$a.unlink()({
-                                'handler': errorLogger().data
+                        'stringsAreEqual': a_diff.$r.stringsAreEqual(),
+                        'validateFile': a_private.$a.validateFile({
+                            'diffData': a_diff.$r.diffData(),
+                            'readFile': () => {
+                                pl.panic("SFSDFSDF")
+                            },
+                        }, {
+                            'unlink': a_fs.$a.createUnlinkFireAndForget({
+                                'unlink': a_fsr.$r.unlink()
+                            })({
+                                'errorHandler': a_fserror.$a.unlink()({
+                                    'handler': errorLogger().data
+                                }),
                             }),
-                        }),
-                        'writeFile': a_fs.$a.createWriteFileFireAndForget({
-                            'createFileWriter': a_fsr.$r.createFileWriter()
-                        })({
-                            'errorHandler': a_fserror.$a.writeFile()({
-                                'handler': errorLogger().data
+                            'writeFile': a_fs.$a.createWriteFileFireAndForget({
+                                'createFileWriter': a_fsr.$r.createFileWriter()
+                            })({
+                                'errorHandler': a_fserror.$a.writeFile()({
+                                    'handler': errorLogger().data
+                                }),
                             }),
                         }),
                     }),
-                }),
-                'serializeSummary': a_analyse.$a.serializeSummary({
-                    'add': a_arithmetic.$r.add(),
-                    'isZero': a_boolean.$r.isZero(),
-                    'negate': a_arithmetic.$r.negate(),
-                }),
-                'serializeTestResult': a_analyse.$a.serializeTestResult({
-                    'dictionaryForEach': a_foreach.$r.createDictionaryForEach({
-                        'compare': a_collation.$r.localeIsABeforeB(),
+                    'serializeSummary': a_analyse.$a.serializeSummary({
+                        'add': a_arithmetic.$r.add(),
+                        'isZero': a_boolean.$r.isZero(),
+                        'negate': a_arithmetic.$r.negate(),
+                    }),
+                    'serializeTestResult': a_analyse.$a.serializeTestResult({
+                        'dictionaryForEach': a_foreach.$r.createDictionaryForEach({
+                            'compare': a_collation.$r.localeIsABeforeB(),
+                        })
+                    }),
+                    'summarize': a_analyse.$a.summarize({
+                        'increment': a_analyse.$a.increment()
                     })
                 }),
-                'summarize': a_analyse.$a.summarize({
-                    'increment': a_analyse.$a.increment()
-                })
-            }),
-            'createTestParametersParser': a_parameters.$a.createTestParametersParser(),
-            'getTestSet': $d.getTestSet,
-        })({
-            'log': logger(),
-            'logErrors': errorLogger(),
-            'reportFailed': failedFlagger(),
-        })
+                'createTestParametersParser': a_parameters.$a.createTestParametersParser(),
+                'getTestSet': $d.getTestSet,
+            })({
+                'log': logger(),
+                'logErrors': errorLogger(),
+                'reportFailed': failedFlagger(),
+            })
+        }
     }
 }
